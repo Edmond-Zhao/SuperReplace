952eeb8484ae27d339fb9d9b707f86a865ddea55
Edmond-Zhao <82437570+Edmond-Zhao@users.noreply.github.com>
2022-08-08 16:21:43 +0800

    Modified mmanage files to stroe a line string in a single node.

--- mmanage.c
+++ mmanage.c
@@ -5,38 +5,7 @@
 #include <string.h>
 #include "mmanage.h"
 
-block_db_t *mmg_get_block_db( const size_t ul_mem_size )
-{
-    block_db_t *p_block_db = NULL;
-    char *p_block = NULL;
-
-    if( ul_mem_size == 0 || ul_mem_size > MAX_BLOCK_SIZE )
-        return NULL;
-
-    p_block_db = ( block_db_t * )malloc( sizeof( block_db_t ) );
-    if( p_block_db == NULL )
-    {
-        printf( "Malloc failed!\n" );
-        return NULL;
-    }
-    memset( p_block_db, 0, sizeof( block_db_t ) );
-    p_block = ( char * )malloc( ul_mem_size );
-    if( p_block == NULL )
-    {
-        printf( "Failed to malloc for block!\n" );
-        free( p_block_db );
-        return NULL;
-    }
-    memset( p_block, 0, ul_mem_size );
-    p_block_db->block_size = ul_mem_size;
-    p_block_db->p_block = p_block;
-    p_block_db->p_block_end = p_block + ul_mem_size - 1;
-    p_block_db->p_w_end = p_block;
-
-    return p_block_db;
-}
-
-static line_node_t *get_node( void )
+static line_node_t *node_get_new( void )
 {
     line_node_t *p_node = malloc( sizeof( line_node_t ) );
     if( p_node )
@@ -45,27 +14,73 @@ static line_node_t *get_node( void )
     return p_node;
 }
 
+static int node_add_line( line_node_t *p_node, const char *s_line )
+{
+    int i_length = 0;
+    
+    if( s_line == NULL || strlen( s_line ) == 0 )
+    {
+        printf( "The line string is unaviable.\n" );
+        return -1;
+    }
+
+    i_length = strlen( s_line );
+    p_node->p_line = ( char* )malloc( i_length + 1 );
+    if( p_node->p_line == NULL )
+    {
+        printf( "Malloc failed!\n" );
+        return -1;
+    }
+    memset( p_node->p_line, 0, i_length + 1 );
+    strncpy( p_node->p_line, s_line, i_length );
+
+    return 0;
+}
+
+static int node_release( line_node_t *p_node )
+{
+    if( p_node )
+    {
+        if( p_node->p_line )
+            free( p_node->p_line );
+        free( p_node );
+    }
+    
+    return 0;
+}
+
+block_db_t *mmg_get_block_db( void )
+{
+    block_db_t *p_block_db = NULL;
+
+    p_block_db = ( block_db_t * )malloc( sizeof( block_db_t ) );
+    if( p_block_db == NULL )
+    {
+        printf( "Malloc failed!\n" );
+        return NULL;
+    }
+    memset( p_block_db, 0, sizeof( block_db_t ) );
+
+    return p_block_db;
+}
+
 int mmg_add_a_line( block_db_t *p_block_db, char *s_line )
 {
     line_node_t *p_node = NULL;
-    int i_line_len = 0;
 
     if( p_block_db == NULL || s_line == NULL )
         return -1;
 
-    i_line_len = strlen( s_line );
-    if( p_block_db->p_w_end + i_line_len > p_block_db->p_block_end )
-    {
-        printf( "The mem db is full, can't be added new lines.\n" );
-        return -1;
-    }
-
-    p_node = get_node();
+    p_node = node_get_new();
     if( p_node == NULL )
         return -1;
-    strcpy( p_block_db->p_w_end, s_line );
-    p_node->p_line = p_block_db->p_w_end;
-    p_block_db->p_w_end = p_block_db->p_w_end + i_line_len + 1;
+    if( node_add_line( p_node, s_line ) != 0 )
+    {
+        node_release( p_node );
+        p_node = NULL;
+        return -1;
+    }
+    
     p_block_db->i_line_node_count++;
     if( p_block_db->p_node_head == NULL )
     {
@@ -110,15 +125,12 @@ int mmg_destroy( block_db_t *p_block_db )
 
     if( p_block_db )
     {
-        if( p_block_db->p_block )
-            free( p_block_db->p_block );
-        
         p_node = p_block_db->p_node_head;
         while( p_node )
         {
             p_temp = p_node;
             p_node = p_node->p_next;
-            free( p_temp );
+            node_release( p_temp );
         }
 
         free( p_block_db );
@@ -133,8 +145,8 @@ int mmg_block_db_dump( block_db_t *p_block_db )
 
     if( p_block_db )
     {
-        printf( "Node count:  %d\n", p_block_db->i_line_node_count );
-        printf( "Block size:  %zu bytes\n", p_block_db->block_size );
+        printf( "Node count:     %d\n", p_block_db->i_line_node_count );
+        printf( "Para file size: %zu\n", p_block_db->file_size );
         printf( "Block content: \n" );
         p_node = p_block_db->p_node_head;
         while( p_node )
